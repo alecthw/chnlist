5681287c1550bad3af692f5b92a211b56da5857c
alecthw <alecthw@163.com>
2023-03-10 17:23:05 +0800

    add

---
+++ script/Auto_Switch.js
@@ -0,0 +1,67 @@
+/*******************************
+
+Surge根据网络自动切换出站模式脚本，指定SSID切换为直连模式
+
+
+******************
+Surge配置说明
+请修改argument中的SSID为需要自动切换到直接连接模式的SSID，以逗号','分隔
+******************
+
+[Script]
+自动模式切换 = type=event, event-name=network-changed, argument="SSID1,SSID2,SSID3", script-path=https://raw.githubusercontent.com/alecthw/chnlist/main/script/Auto_Switch.js
+
+*******************************/
+
+
+
+const direct_ssids = [];
+
+$argument.split(',').forEach(item => {
+    direct_ssids.push(item.trim())
+});
+
+console.log($argument);
+console.log(JSON.stringify(direct_ssids));
+
+function switchOutbound(mode) {
+    console.log(`Target mode: ${mode}`);
+
+    $httpAPI('GET', '/v1/outbound', undefined, function (curRes) {
+        console.log(`Current mode: ${curRes.mode}`);
+
+        if (curRes.mode === mode) {
+            console.log('Switch is not need!');
+            $done();
+
+        } else {
+            $httpAPI('POST', '/v1/outbound', { mode }, function (postRes) {
+                console.log(`Post Res: ${JSON.stringify(postRes)}`);
+
+                $httpAPI('GET', '/v1/outbound', undefined, function (switchRes) {
+                    console.log(`Switched mode: ${switchRes.mode}`);
+
+                    if (switchRes.mode === mode) {
+                        console.log('Switch successful!');
+                    } else {
+                        console.log('Switch failed!');
+                        $notification.post('自动策略切换', '', '自动出站模式切换失败，请检查日志。');
+                    }
+
+                    $done();
+                }
+                );
+            });
+        }
+    });
+}
+
+console.log(`Network Change: ${JSON.stringify($network)}`);
+
+const ssid = $network.wifi.ssid;
+
+if (ssid && direct_ssids.includes(ssid)) {
+    switchOutbound('direct');
+} else {
+    switchOutbound('rule');
+}
